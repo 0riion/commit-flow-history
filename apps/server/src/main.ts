82fde40cf607f0b60203c877449f11639f4e6e40
Julio Flores <juliocesarflores12@gmail.com>
2023-08-24 18:23:16 -0400

    refactor: environment variables

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import environment from '../config/env.config';
 
-const PORT = process.env.PORT || 4000;
+const PORT = environment().port || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
